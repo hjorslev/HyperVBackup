8eb693f81ef6fcaccedc92fea4a53d4b940e77fb
Frederik Hjorslev <frederik@hjorslev.com>
2019-07-10 16:07:16 +0200

    add PSScriptAnalyzer

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-[string[]]$PowerShellModules = @("Pester", "posh-git", "platyPS", "InvokeBuild", "7Zip4Powershell")
+[string[]]$PowerShellModules = @('Pester', 'posh-git', 'platyPS', 'InvokeBuild', '7Zip4Powershell', 'PSScriptAnalyzer')
 [string[]]$PackageProviders = @('NuGet', 'PowerShellGet')
 [string[]]$ChocolateyPackages = @('mkdocs')
 
