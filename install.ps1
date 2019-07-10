a235e03ef1d6c3be2d9b0726ecce8af1107a3ddc
Frederik Hjorslev <frederik@hjorslev.com>
2019-07-10 21:46:48 +0200

    add Configuration package

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-[string[]]$PowerShellModules = @('Pester', 'posh-git', 'platyPS', 'InvokeBuild', '7Zip4Powershell', 'PSScriptAnalyzer')
+[string[]]$PowerShellModules = @('Pester', 'posh-git', 'platyPS', 'InvokeBuild', '7Zip4Powershell', 'PSScriptAnalyzer', 'Configuration')
 [string[]]$PackageProviders = @('NuGet', 'PowerShellGet')
 [string[]]$ChocolateyPackages = @('mkdocs')
 
